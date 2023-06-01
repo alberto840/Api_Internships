4027b32d7a163df4525d971465634ca8ae174f47
Alberto840 <alberto.flores@ucb.edu.bo>
2023-06-01 16:26:20 -0400

    acabando student

--- src/main/java/bo/edu/ucb/crud/Student/StudentController.java
+++ src/main/java/bo/edu/ucb/crud/Student/StudentController.java
@@ -2,8 +2,12 @@ package bo.edu.ucb.crud.Student;
 
 import java.util.List;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +25,11 @@ public class StudentController {
     @GetMapping
     public List<StudentEntity> getStudents(){
         return studentService.getStudents();
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> registerNewStudent(@RequestBody StudentEntity student){
+        return this.studentService.addNewStudents(student);
     }
     
 }
--- src/main/java/bo/edu/ucb/crud/Student/StudentEntity.java
+++ src/main/java/bo/edu/ucb/crud/Student/StudentEntity.java
@@ -1,10 +1,22 @@
 package bo.edu.ucb.crud.Student;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
 public class StudentEntity {
     //Atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer studentId;
     private String firstName;  
     private String lastName;
+    @Column(unique = true)
     private String email;
     private String username;
     private String password;
