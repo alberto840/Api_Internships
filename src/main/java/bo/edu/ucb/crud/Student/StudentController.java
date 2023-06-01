71cce128c0cae787e80a6f2b9b16c02bf06403bc
Alberto840 <alberto.flores@ucb.edu.bo>
2023-06-01 16:45:06 -0400

    creando espacios para nuevos objetos

--- src/main/java/bo/edu/ucb/crud/Student/StudentController.java
+++ src/main/java/bo/edu/ucb/crud/Student/StudentController.java
@@ -2,7 +2,6 @@ package bo.edu.ucb.crud.Student;
 
 import java.util.List;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
